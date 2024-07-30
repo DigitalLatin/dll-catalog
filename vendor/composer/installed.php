151536fbb789bec91bb3f763c8bfdc1083eeea94
Sam Huskey <sjhuskey@hobbes.oscer.ou.edu>
2024-07-30 13:32:34 -0500

    codebase + database + solr working

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'drupal/recommended-project',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'dd35598d6c36ccd6728947b9b9ec10d592c5bb96',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -546,9 +546,9 @@
             'dev_requirement' => false,
         ),
         'drupal/recommended-project' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'dd35598d6c36ccd6728947b9b9ec10d592c5bb96',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
